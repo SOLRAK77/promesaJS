24812ba30bdd15b41126356a611b6486a6467ba8
Carlos Castillo <ccastillo@GMPTI.local>
2020-03-04 04:31:53 -0600

    emulando una promesa

--- project/app/rest-controller.js
+++ project/app/rest-controller.js
@@ -3,9 +3,10 @@
 var app = require('./app');
 
 
-var restController = function($scope, $http){
+var restController = function($scope, $http, $timeout){
 
     $scope.data;
+    $scope.process = "procesing.."
 
     $http({
         method:'GET',
@@ -13,13 +14,18 @@ var restController = function($scope, $http){
     })
     .then(function(response)
         {
-            console.log(response.data);
-            $scope.data = response.data;
+            $timeout(function(){
+                console.log(response.data);
+                $scope.data = response.data;                
+                $scope.process = "processed";
+            },2000)
         }
     );
 
+    $scope.process = "almost there"
+
 };
 
-app.controller("restController",['$scope','$http',restController]);
+app.controller("restController",['$scope','$http','$timeout',restController]);
 
 module.exports= app;
